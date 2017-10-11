cab36ea1a5a946a24c97dbfb47c1d62266f36181
Geert Roks <geertrocks@gmail.com>
2017-10-11 10:46:47 +0200

    Moved everything to the CSD2a folder.

---
+++ AntwoordenExamples/01_playASoundAntw.py
@@ -0,0 +1,57 @@
+#simpleaudio is imported as sa -> shorter name
+import simpleaudio as sa
+
+"""
+An example project in which three wav files are played after eachother.
+
+------ HANDS-ON TIPS ------
+- Answer the following question before running the code:
+  Do you expect to hear the samples played simultaniously or after eachother?
+  Why?
+
+- Alter the code:
+  Play the sounds simultaniously.
+
+- Alter the code:
+  Ask the user to choice which one of the three samples should be played and
+  only the chosen sample.
+"""
+
+#load 3 audioFiles
+sampleHigh = sa.WaveObject.from_wave_file("../Examples/pythonExamples/audio/01_audioFiles/audioFiles/Pop.wav")
+sampleMid = sa.WaveObject.from_wave_file("../Examples/pythonExamples/audio/01_audioFiles/audioFiles/Laser1.wav")
+sampleLow = sa.WaveObject.from_wave_file("../Examples/pythonExamples/audio/01_audioFiles/audioFiles/Dog2.wav")
+
+x = True
+#ask for input
+while x:
+    choice = input("What sample would you like to hear? Type 1, 2 or 3. -> ")
+    if choice.isdigit():
+        choice = int(choice)
+        x = False
+
+    else:
+        print("Please type 1, 2 or 3.")
+
+
+if choice == 1:
+    #play high sample
+    sampleHighPlay = sampleHigh.play()
+    #wait till sample is done playing
+    #sampleHighPlay.wait_done()
+
+if choice == 2:
+    #play mid sample
+    sampleMidPlay = sampleMid.play()
+    #wait till sample is done playing
+    #sampleMidPlay.wait_done()
+
+if choice == 3:
+    #play low sample
+    sampleLowPlay = sampleLow.play()
+    #wait till sample is done playing
+    #sampleLowPlay.wait_done()
+
+else :
+    print("Please type 1, 2 or 3.")
+    x = True
