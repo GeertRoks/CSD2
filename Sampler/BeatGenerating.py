c57582bbf37f5fb8bd32cb966a00ffb044556238
Geert Roks <geertrocks@gmail.com>
2017-10-23 14:50:52 +0200

    HiHat algorithm, Input questions moved to UI.py

--- Sampler/BeatGenerating.py
+++ Sampler/BeatGenerating.py
@@ -26,9 +26,21 @@ def SnareGen(kseq, amountOfSixteenths):
 
 def HiHatGen(amountOfSixteenths):
     seq = []
-    for i in range(0, amountOfSixteenths):
-        seq.append(i)
+    rndchoice = random.randint(0, 3)
+    if rndchoice == 0:  #sixteenth not HiHat
+        for i in range(amountOfSixteenths):
+            seq.append(i)
+    elif rndchoice == 1: #eight note HiHat
+        for i in range(amountOfSixteenths):
+            test = (i + 2) / 2
+            print(test)
+            if test.is_integer():
+                seq.append(i)
+    else:                #offbeat eight note HiHat
+        for i in range(amountOfSixteenths):
+            test = (i + 1) / 2
+            print(test)
+            if test.is_integer():
+                seq.append(i)
     seq = sorted(seq)
-
-HiHat_seq = HiHatGen(20)
-print(HiHat_seq)
+    return seq
--- Sampler/IrregularBeatGenerator.py
+++ Sampler/IrregularBeatGenerator.py
@@ -1,88 +1,31 @@
-import simpleaudio as sa
 import time
-import BeatGenerating as bg
 
+import BeatGenerating as bg
+import UI
+
+UI.startUp()
 
 #input questions
     #time signature
-while True:
-    try:
-        timesig = input("What time signature would you like to hear? --> ")
-        timesig = timesig.strip().split("/") #timesig is now a list with [beats, count]
-
-        beatUnit = int(timesig[1]) #Check Beat Unit
-        if beatUnit != 2 and beatUnit != 4 and beatUnit != 8 and beatUnit != 16:
-            print("Invalid beat unit. The beat unit has to be 2, 4, 8 or 16. Please enter one of the valid options. \n")
-            continue
-
-        beatsPerMeasure = int(timesig[0])  #Check Beats per meassure
-        if beatsPerMeasure < 2 or  beatsPerMeasure > 12:
-            print("Invalid beats per measure. The amount of beats per measure should be between 2 and 12.\n")
-            continue
-        else:
-            break
-
-    except:
-        print("Invalid time signature. Please enter a valid time signature. For example: 7/8 \n" )
-
-        continue
+beatsPerMeasure, beatUnit = UI.timeSig()
 
     #bpm
-while True:
-    bpm = input("How many beats per minute? --> ")
-    if bpm.isdigit() and int(bpm) >= 40 and int(bpm) <= 300:
-        bpm = int(bpm)
-        beatInterval = (240/beatUnit)/(bpm) #bpm conversion, interval for beatUnit
-        sixteenInterval = 15/(bpm) #interval of a sixteenth note
-        measureInterval = beatsPerMeasure  * beatInterval #interval of a measure
-        amountOfSixteenths = beatsPerMeasure * (16/beatUnit)
-        break
-    else:
-        print("Invalid response. Please enter a value between 40 and 300.")
-
+measureInterval, sixteenInterval, amountOfSixteenths = UI.BPM(beatsPerMeasure, beatUnit)
 
     #drumkit TODO: Choose and download sample kits
-while True:
-    print("1. First selection")     #jazz kit
-    print("2. Second selection")    #Rock kit
-    print("3. Third selection")     #808
-    print("4. Fourth Selection")    #African?
-    drumkit = input("what kit would you like to play the beat on? --> ")
-    if drumkit.isdigit() and int(drumkit) >= 1 and int(drumkit) <= 4:
-        drumtkit = int(drumkit)
-        break
-    else:
-        print("Invalid response. Please enter a value between 1 and 4")
-        continue
-if drumkit == 1:
-    samples = [ sa.WaveObject.from_wave_file("Kick.wav"),
-                sa.WaveObject.from_wave_file("Snare.wav"),
-                sa.WaveObject.from_wave_file("Tom.wav"),    ]
-else:
-    if drumkit == 2:
-        samples = [ sa.WaveObject.from_wave_file("Kick.wav"),
-                    sa.WaveObject.from_wave_file("Snare.wav"),
-                    sa.WaveObject.from_wave_file("Tom.wav"),    ]
-    else:
-        if drumkit == 3:
-            samples = [ sa.WaveObject.from_wave_file("Kick.wav"),
-                        sa.WaveObject.from_wave_file("Snare.wav"),
-                        sa.WaveObject.from_wave_file("Tom.wav"),    ]
-        else:
-            samples = [ sa.WaveObject.from_wave_file("Kick.wav"),
-                        sa.WaveObject.from_wave_file("Snare.wav"),
-                        sa.WaveObject.from_wave_file("Tom.wav"),    ]
+samples = UI.drumkitSelection()
 
-
-events = []
-
+#Beat Generating
 #TODO: Random beat generating
 Kick_seq =  bg.KickGen([0, 8], amountOfSixteenths)
-Snare_seq = bg.SnareGen(Kick_seq, amountOfSixteenths)      #number represents place in grid
-Tom_seq =   []
+Snare_seq = bg.SnareGen(Kick_seq, amountOfSixteenths)
+HiHat_seq =  bg.HiHatGen(amountOfSixteenths)
 
 print(Kick_seq)
 print(Snare_seq)
+print(HiHat_seq)
+
+events = []
 
 #transform the sixteenthNoteSequece to an eventlist with time values
 for sixteenIndex in Kick_seq:
@@ -91,10 +34,11 @@ for sixteenIndex in Kick_seq:
 for sixteenIndex in Snare_seq:
   events.append([sixteenIndex * sixteenInterval, 1])
 
-for sixteenIndex in Tom_seq:
+for sixteenIndex in HiHat_seq:
   events.append([sixteenIndex * sixteenInterval, 2])
-
 events.sort() #sort events list so everything is in line to play at the right time
+
+#Playback
 copyEvents = list(events) #events copy for repeating
 event =events.pop(0)
 t0 = time.time()    #save starting time for refrence
