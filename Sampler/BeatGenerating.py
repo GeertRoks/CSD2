7a61b72cd2c44d62a91254ef9cba38bb6ed3ae1f
Geert Roks <geertrocks@gmail.com>
2017-10-24 14:14:57 +0200

    Init MakeMidi, threading not working correctly. Added some Comments

--- Sampler/BeatGenerating.py
+++ Sampler/BeatGenerating.py
@@ -2,74 +2,47 @@ import random
 import time
 
 
-def EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq):
-    events = []
-
-    #transform the sixteenthNoteSequece to an eventlist with time values
-    for sixteenIndex in Kick_seq:
-      events.append([sixteenIndex * sixteenInterval, 0])
-
-    for sixteenIndex in Snare_seq:
-      events.append([sixteenIndex * sixteenInterval, 1])
-
-    for sixteenIndex in HiHat_seq:
-      events.append([sixteenIndex * sixteenInterval, 2])
-    events.sort() #sort events list so everything is in line to play at the right time
-
-    return events
-
-
-#Beat Generating
-def GenerateBeat(amountOfSixteenths):
-    #TODO: Improve kick and snare algorithm
-    Kick_seq =  KickGen([0, 8], amountOfSixteenths)
-    Snare_seq = SnareGen(Kick_seq, amountOfSixteenths)
-    HiHat_seq =  HiHatGen(amountOfSixteenths)
-
-    print(Kick_seq)
-    print(Snare_seq)
-    print(HiHat_seq)
-
-    return Kick_seq, Snare_seq, HiHat_seq
-
-#PlaybackThread
-def PlaybackThread(events, samples, measureInterval):
-    #Playback init
-    copyEvents = list(events) #events copy for repeating
-    event =events.pop(0)
-    t0 = time.time()    #save starting time for refrence
-    while True:
-        currentTime = time.time()
-        if currentTime - t0 >= event[0]: #Check if it's time to play a sample
-            samples[event[1]].play()
-
-            if events:  #if there are events left in the events list
-                event = events.pop(0)
-            else:   #list is empty, wait untill measure is done, then refill
-                while True:
-                    currentTime = time.time()
-                    if currentTime - t0 >= measureInterval:
-                        events = list(copyEvents)
-                        event = events.pop(0)
-                        t0 = time.time()
-                        break
-                    else:
-                        time.sleep(0.01)
-                        continue
-
-        else:
-            time.sleep(0.01)
-
 #Kick Generator
-def KickGen(seq, amountOfSixteenths):
-    while True:
-        rndnote = random.randint(0, (amountOfSixteenths - 1))
-        if rndnote in seq:
-            continue
-        else:
-            break
-    seq.append(rndnote)
-    seq = sorted(seq)
+def KickGen(amountOfSixteenths):
+    #choose how many kicks per measure
+    rndchoice = random.randint(0, 8)
+    x = 0
+    if amountOfSixteenths <= 12:        #short measures
+        seq = [0]
+        if rndchoice < 4:
+            x = 1
+        elif rndchoice >= 4 and rndchoice < 7:
+            x = 2
+        elif rndchoice == 7:
+            x = 3
+    elif amountOfSixteenths > 12 and amountOfSixteenths <= 20:  #middle measures
+        seq = [0, 8]
+        if rndchoice < 4:
+            x = 2
+        elif rndchoice >= 4 and rndchoice < 7:
+            x = 3
+        elif rndchoice == 7:
+            x = 4
+    elif amountOfSixteenths > 20:       #long measures
+        seq = [0, 8]
+        if rndchoice < 4:
+            x = 3
+        elif rndchoice >= 4 and rndchoice < 7:
+            x = 4
+        elif rndchoice == 7:
+            x = 5
+    x = x - len(seq)
+    print(rndchoice)
+    print(x)
+    for i in range(0, x):
+        while True:
+            rndnote = random.randint(0, (amountOfSixteenths - 1))
+            if rndnote in seq:
+                continue
+            else:
+                break
+        seq.append(rndnote)
+        seq = sorted(seq)
     return seq
 
 
@@ -98,13 +71,11 @@ def HiHatGen(amountOfSixteenths):
     elif rndchoice == 1: #eight note HiHat
         for i in range(amountOfSixteenths):
             test = (i + 2) / 2
-            print(test)
             if test.is_integer():
                 seq.append(i)
     else:                #offbeat eight note HiHat
         for i in range(amountOfSixteenths):
             test = (i + 1) / 2
-            print(test)
             if test.is_integer():
                 seq.append(i)
     seq = sorted(seq)
--- Sampler/IrregularBeatGenerator.py
+++ Sampler/IrregularBeatGenerator.py
@@ -5,6 +5,72 @@ import UI
 
 UI.startUp()
 
+global beatsPerMeasure,beatUnit, amountOfSixteenths, measureInterval, sixteenInterval, samples, events
+
+
+#Transfer random beat to an Event list
+def EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq):
+    events = []
+
+    #transform the sixteenthNoteSequece to an eventlist with time values
+    for sixteenIndex in Kick_seq:
+      events.append([sixteenIndex * sixteenInterval, 0])
+
+    for sixteenIndex in Snare_seq:
+      events.append([sixteenIndex * sixteenInterval, 1])
+
+    for sixteenIndex in HiHat_seq:
+      events.append([sixteenIndex * sixteenInterval, 2])
+    events.sort() #sort events list so everything is in line to play at the right time
+
+    return events
+
+
+#Beat Generating
+def GenerateBeat(amountOfSixteenths):
+    #TODO: Improve snare algorithm
+    Kick_seq =  bg.KickGen(amountOfSixteenths)
+    Snare_seq = bg.SnareGen(Kick_seq, amountOfSixteenths)
+    HiHat_seq =  bg.HiHatGen(amountOfSixteenths)
+
+    print(Kick_seq)
+    print(Snare_seq)
+    print(HiHat_seq)
+
+    return Kick_seq, Snare_seq, HiHat_seq
+
+#PlaybackThread
+def PlaybackThread():
+    global events, samples, measureInterval
+
+    #Playback init
+    copyEvents = list(events) #events copy for repeating
+    event = events.pop(0)
+    t0 = time.time()    #save starting time for refrence
+    while True:
+        currentTime = time.time()
+        if currentTime - t0 >= event[0]: #Check if it's time to play a sample
+            samples[event[1]].play()
+
+            if events:  #if there are events left in the events list
+                event = events.pop(0)
+            else:   #list is empty, wait untill measure is done, then refill
+                while True:
+                    currentTime = time.time()
+                    if currentTime - t0 >= measureInterval:
+                        events = list(copyEvents)
+                        event = events.pop(0)
+                        t0 = time.time()
+                        break
+                    else:
+                        time.sleep(0.01)
+                        continue
+
+        else:
+            time.sleep(0.01)
+
+
+
 #input questions
     #time signature
 beatsPerMeasure, beatUnit, amountOfSixteenths = UI.timeSig()
@@ -15,12 +81,15 @@ measureInterval, sixteenInterval= UI.BPM(beatsPerMeasure, beatUnit)
     #drumkit TODO: Choose and download sample kits
 samples = UI.drumkitSelection()
 
-Kick_seq, Snare_seq, HiHat_seq = bg.GenerateBeat(amountOfSixteenths)
-events = bg.EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq)
 
-#Thread during playback
+#Beat Generating
+Kick_seq, Snare_seq, HiHat_seq = GenerateBeat(amountOfSixteenths)
+events = EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq)
+
+#Input Thread during playback
+#TODO:
 try:
-   _thread.start_new_thread(bg.PlaybackThread, (events, samples, measureInterval))
+   _thread.start_new_thread(PlaybackThread, ())
 except:
    print("Error: unable to start thread \n")
 
@@ -37,7 +106,7 @@ while True:
      # BPM
     if userInput[0].lower() == "bpm":
         measureInterval, sixteenInterval= UI.BPM(beatsPerMeasure, beatUnit)
-        events = bg.EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq)
+        events = EventList(sixteenInterval, Kick_seq, Snare_seq, HiHat_seq)
 
 
     # Exit program
